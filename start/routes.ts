727a1eeadf8936d2c2e7b250585a960ecc4b14dc
Abdallah Hegab <abdallahhegab192@gmail.com>
2022-07-27 14:57:51 +0200

    added / route

--- start/routes.ts
+++ start/routes.ts
@@ -21,6 +21,7 @@
 import Route from '@ioc:Adonis/Core/Route';
 
 Route.group(()=>{
+  Route.post('/',()=>{});
   Route.post('/register','AuthController.register');
   Route.post('/login','AuthController.login');
   Route.post('/logout','AuthController.logout');
