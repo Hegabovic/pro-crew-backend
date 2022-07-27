73040bf55ed260f6688f312c8f95a1761688cf4f
Abdallah Hegab <abdallahhegab192@gmail.com>
2022-07-27 16:20:20 +0200

    added procfile

--- start/routes.ts
+++ start/routes.ts
@@ -20,10 +20,11 @@
 
 import Route from '@ioc:Adonis/Core/Route';
 
+Route.get('/',()=>{
+  return 'Hello world'
+});
 Route.group(()=>{
-  Route.get('/',()=>{
-    return 'Hello world'
-  });
+
   Route.post('/register','AuthController.register');
   Route.post('/login','AuthController.login');
   Route.post('/logout','AuthController.logout');
